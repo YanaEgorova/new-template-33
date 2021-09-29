9fb6f0b8521bc44331216c74175db5c67d25d67c
Yana <yanaegorova69.3@gmail.com>
2021-09-29 18:50:45 +0300

    +

---
+++ js/data/products.js
@@ -0,0 +1,143 @@
+export const products = [
+    {
+        id: 'product-1',
+        name: 'Spatula',
+        price: 6.96,
+        image: './img/img1.png',
+        description: [
+            {
+                text: 'Stainless steel construction prevents rust while a plastic handle provides comfort.',
+            },
+            {
+                text: '13 Inch length provides enough distance from the pan to prevent getting burnt while also enough control to flip foods easily.',
+            },
+            {
+                text: 'Thin, angled edge slips under food easily when cooking for an optimized experience.',
+            },
+            {
+                text: 'Dishwasher safe provides quick and easy cleanup when cooking with any foods.',
+            },
+            {
+                text: 'Constructed without welding points to create strength and durability to the turner.',
+            },
+        ],
+        images: ['./img/img1-1.png', './img/img1-2.png'],
+    },
+    {
+        id: 'product-2',
+        name: 'Puppy Stuffed Animal',
+        price: 29.99,
+        image: './img/img2.png',
+        description: [
+            {
+                text: 'Lifelike yellow lab puppy plush stuffed animal with soft beige coat.',
+            },
+            {
+                text: 'The super-cute baby Lab measures approximately 11 inches tall and about 2 feet long.',
+            },
+            {
+                text: 'Realistic details include soft ears, a pet-able beige coat, waggy tail, and a friendly expression.',
+            },
+            {
+                text: 'Soft, huggable body, quality craftsmanship, strong and sturdy interior structure.',
+            },
+            {
+                text: 'Makes a great gift for toddlers to adults, ages 3 and older, for hands-on, screen-free play.',
+            },
+        ],
+        images: ['./img/img2-1.jpg', './img/img2-2.jpg'],
+    },
+    {
+        id: 'product-3',
+        name: 'Prop Replica',
+        price: 64.90,
+        image: './img/img3.png',
+        description: [
+            {
+                text: 'Based on the 1990 film Teenage Mutant Ninja Turtles.',
+            },
+            {
+                text: 'Hand painted replica of Casey Jones\' mask.',
+            },
+            {
+                text: 'Features: adjustable straps that allow you to wear or display the mask.',
+            },
+            {
+                text: 'Made of heavy - duty ABS plastic.',
+            },
+            {
+                text: 'Closed box packaging.',
+            },
+        ],
+        images: ['./img/img3-1.png', './img/img3-2.png'],
+    },
+    {
+        id: 'product-4',
+        name: 'Active Learning Stool',
+        price: 98.90,
+        image: './img/img4.png',
+        description: [
+            {
+                text: 'Easy-to-use pneumatic gas lift adjusts the seat height from 18 1/2 to 28 inches.',
+            },
+            {
+                text: 'The non-slip, curved base features a wide range of motion.Weight Capacity:265 Pounds.',
+            },
+            {
+                text: 'Backless design encourages proper posture while strengthening core muscles.',
+            },
+            {
+                text: 'The 12 5/8"-wide seat is constructed from easy-to-clean vinyl-covered foam.',
+            },
+            {
+                text: 'Lightweight design for easy mobility and storage.',
+            },
+            {
+                text: 'Seat Material: Polyurethane molded foam w/ vinyl upholstery.',
+            },
+        ],
+        images: ['./img/img4-1.png', './img/img4-2.png', './img/img4-3.png'],
+    },
+    {
+        id: 'product-5',
+        name: 'Floor Lamp',
+        price: 119.98,
+        image: './img/img5.png',
+        description: [
+            {
+                text: 'Unique pull shade that adjusts the beam of light from 3-Inch to 33-Inch at a distance of 12-Inch, ideal for reading, crafting and other hobbies.',
+            },
+            {
+                text: '15.5 inch adjustable gooseneck directs tight light beam with little spill to point light where you need it most; energy-efficient, clarity enhancing 5W LED bulb produces crisp white light that’s over 7X brighter than a 60W incandescent bulb (at 12").',
+            },
+            {
+                text: 'Sleek, contemporary design features a steel pole and base, anodized aluminum shade available in Black, Nickel and Gold; Measures 52 inches in height.',
+            },
+        ],
+        images: ['./img/img5-1.png', './img/img5-2.png', './img/img5-3.png'],
+    },
+    {
+        id: 'product-6',
+        name: 'Single Door Pantry',
+        price: 148.00,
+        image: './img/img6.png',
+        description: [
+            {
+                text: 'Create a solution for your limited storage space with the Single Door Storage Pantry Cabinet.',
+            },
+            {
+                text: 'Use this Cabinet in your kitchen, dining room or laundry room for extra concealed storage with its 4 shelves.',
+            },
+            {
+                text: 'Two adults are required for proper assembly. Assembled dimensions: 60.25"H x 18.1875"W x 15.6875"D.',
+            },
+            {
+                text: 'The Old Fashioned Pine finish, along with the wainscoting detail on the door front gives this piece a charming country feel.',
+            },
+            {
+                text: 'The Single Door Storage Pantry Cabinet is constructed of laminated particleboard.',
+            },
+        ],
+        images: ['./img/img6-1.png','./img/img6-2.png', './img/img6-3.png', './img/img6-4.png', './img/img6-5.png', './img/img6-6.png', './img/img6-7.png', './img/img6-8.png'],
+    },
+]
